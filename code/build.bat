4d55c0e62614e563c16edf98b1bab24bbdebaa26
Mohit Ishpunyani <60349301+16mi@users.noreply.github.com>
2025-06-20 15:48:24 +0530

    Inital commit

---
+++ code/build.bat
@@ -0,0 +1,29 @@
+@echo off
+
+:: Setup environment for VS 2022 (adjust path to your install if needed)
+call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
+
+set CommonCompilerFlags=-MT -nologo -Gm- -GR- -EHa- -Od -Oi -WX -W4 -wd4201 -wd4100 -wd4189 -FC -Z7
+set CommonLinkerFlags=-opt:ref user32.lib gdi32.lib
+
+REM TODO - can we just build both with one exe?
+
+IF NOT EXIST ..\build mkdir ..\build
+pushd ..\build
+
+echo Compiling...
+REM 32-bit build
+REM cl %CommonCompilerFlags% ..\code\refloader.cpp /link -subsystem:CONSOLE %CommonLinkerFlags%
+
+echo Output...
+REM 64-bit build
+cl %CommonCompilerFlags% ..\code\refloader.cpp /link %CommonLinkerFlags%
+
+echo Done. Checking for output...
+if exist refloader.exe (
+    echo SUCCESS: refloader.exe created.
+) else (
+    echo ERROR: No .exe file generated.
+)
+
+popd
---
+++ code/refloader.cpp
@@ -0,0 +1,40 @@
+#include <winnt.h>
+#include <stdio.h>
+#include <stdint.h>
+
+typedef enum _LoaderStatus {
+    LoaderSuccess,
+    LoaderFailed,
+    LoaderBadFormat
+} LoaderStatus;
+
+// For contextual information that loader will need
+//typedef struct _loader_ctx {
+//    void *p;
+//    size_t size;
+//} loader_ctx;
+
+LoaderStatus
+validate_dos_header(const unsigned char* buf, unint32_t size) {
+    if(buff == NULL || size < sizeof(IMAGE_DOS_HEADER)) return LoaderBadFormat;
+    if(buff[0] != 'M' || buff[1] != 'Z') return LoaderBadFormat;
+
+    // Using uintptr_t to perform safety checks during pointer arithmetic operations
+    // NOTE: uintptr_t can also overflow, to be more secure only use offsets
+    uintptr_t base = (uintptr_t)buf;
+    if(base + ((const IMAGE_DOS_HEADER*)buf)->e_lfanew > (base + size)) return LoaderBadFormat;
+
+    return LoaderSuccess;
+}
+
+LoaderStatus
+reflective_loadlibrary(loader_ctx* ctx, const void* buffer, unint32_t size) {
+    LoaderStatus status = LoaderSuccess;
+    unsigned char* cbuffer = (unsigned char*)buffer;
+    
+    if(validate_dos_header(cbuffer, size) == LoaderFailed) {
+        return LoaderFailed;
+    }
+
+    return status;
+}
