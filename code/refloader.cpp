def5e4c4e7a3e3a1ef7846964f98f97b11564e64
Mohit Ishpunyani <60349301+16mi@users.noreply.github.com>
2025-06-24 22:40:17 +0530

    Allocating space

--- code/refloader.cpp
+++ code/refloader.cpp
@@ -1,21 +1,31 @@
-#include <winnt.h>
+#ifdef _M_X64
+#define _AMD64_
+#define CURRENT_ARCH  IMAGE_FILE_MACHINE_AMD64
+#else
+#define _X86_
+#define CURRENT_ARCH  IMAGE_FILE_MACHINE_I386
+#endif
+
+#include <windows.h>
 #include <stdio.h>
 #include <stdint.h>
 
 typedef enum _LoaderStatus {
     LoaderSuccess,
     LoaderFailed,
-    LoaderBadFormat
+    LoaderBadFormat,
+    LoaderInvalid,
+    LoaderNoMem
 } LoaderStatus;
 
-For contextual information that loader will need
+// For contextual information that loader will need
 typedef struct _loader_ctx {
     void *p;
     size_t size;
 } loader_ctx;
 
 LoaderStatus
-validate_dos_header(const unsigned char* buff, unint32_t size) {
+validate_dos_header(const unsigned char* buff, uint32_t size) {
     if(buff == NULL || size < sizeof(IMAGE_DOS_HEADER)) return LoaderBadFormat;
     if(buff[0] != 'M' || buff[1] != 'Z') return LoaderBadFormat;
 
@@ -28,13 +38,37 @@ validate_dos_header(const unsigned char* buff, unint32_t size) {
 }
 
 LoaderStatus
-reflective_loadlibrary(loader_ctx* ctx, const void* buffer, unint32_t size) {
+reflective_loadlibrary(loader_ctx* ctx, const void* buffer, uint32_t size) {
     LoaderStatus status = LoaderSuccess;
     unsigned char* cbuffer = (unsigned char*)buffer;
+    const IMAGE_NT_HEADERS* nt = NULL;
+    unsigned char* new_buffer = NULL;
+    uint32_t nb_size = 0;
+
+    if(ctx == NULL) return LoaderInvalid;
     
     if(validate_dos_header(cbuffer, size) == LoaderFailed) {
         return LoaderFailed;
     }
 
+    nt = (const IMAGE_NT_HEADERS*)(cbuffer + ((const IMAGE_DOS_HEADER*)buffer)->e_lfanew);
+
+    if((nb_size = nt->OptionalHeader.SizeOfImage) == 0) return LoaderBadFormat;
+
+    new_buffer = (unsigned char*)VirtualAlloc(NULL,
+                      nb_size,
+                      MEM_COMMIT|MEM_RESERVE,
+                      PAGE_EXECUTE_READWRITE);
+    if(new_buffer == NULL) return LoaderNoMem;
+
+    // Cleanup
+    if(status != LoaderSuccess) {
+        VirtualFree(new_buffer, 0, MEM_RELEASE);
+    }
+    
     return status;
+}
+
+int main() {
+    return 0;
 }
